8de92c0e11c5fad039c16f175c752ffce4fc8e9d
Nicol Mouton <nicolmouton@yahoo.com>
2022-05-07 13:53:04 +1000

    updated README, added comments to server js. Completed implementing delete method

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# express-note-taker
+
+ # Express Note Taker
+
+  Table of content
+
+  
+  [Description](#description)
+
+  [License](#license)
+
+  [Installation](#installation)
+
+  [Usage](#usage)
+
+  [Contribute](#contribute)
+
+  [Questions](#questions)
+
+  
+  ## Description 
+  
+  * The Express Note Taker is a simple note taking application where allows you to take notes, view notes, delete notes and save notes across reloads. Data for this project is persisted in a database.
+
+  ## License
+  
+  
+  * MIT License
+  
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+  
+  ## Installation
+  
+  
+  *This application is hosted on Heroku at => {insert heroku link}, or git clone from Github Repo.
+
+  ## Usage
+  
+  
+  * Enter notes in the indicated fields, press the save icon to store notes. Please on note titles in the left hand column to view stored notes. 
+
+  ## Contribute
+  
+  
+  * Contribute via pull requests with detailed README files. Alternatively get in touch via email!
+
+  ## Questions!
+  
+  * 👩‍💻 Github.com/Nicolmmm
+  * ✉️Nicolmouton@yahoo.com
+  
--- server.js
+++ server.js
@@ -4,23 +4,25 @@ const fs = require("fs");
 const path = require("path");
 const db = require("./db/db.json");
 
+//start port at 3001 or variable location
 const PORT = process.env.PORT || 3001;
 
+//use Express.js, express JSON, use public folder for statis files.
 const app = express();
-
 app.use(express.json());
-
 app.use(express.static("public"));
 
+//handle path for note taking page
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
+//gets notes from database
 app.get("/api/notes", (req, res) => {
-  console.log("req made");
   res.json(db);
 });
 
+//adds new notes to database.
 app.post("/api/notes", (req, res) => {
   res.json(`${req.method} request recieved`);
 
@@ -33,9 +35,7 @@ app.post("/api/notes", (req, res) => {
       } else {
         //assign random id to object
         content.id = Math.floor(Math.random() * 1000000000000 + 1);
-
         let parsedData = JSON.parse(data);
-        console.log(parsedData);
         parsedData.push(content);
 
         writeToFile(file, parsedData);
@@ -53,9 +53,11 @@ app.post("/api/notes", (req, res) => {
   readAppend(req.body, "./db/db.json");
 });
 
+//delete method
 app.delete("/api/notes/:id", (req, res) => {
   res.status(200).send("recieved Delete request");
   const toDelete = req.params.id;
+  //loops through object array to match ID with object and removes object with splice.
   for (let index = 0; index < db.length; index++) {
     const element = db[index];
     if (element.id == toDelete) {
