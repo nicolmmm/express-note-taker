9de01fdf82caeca2662704452835dd6fe9652f79
Nicol Mouton <nicolmouton@yahoo.com>
2022-05-09 18:10:28 +1000

    all server methods are working, application appears to meet minimum acceptance criteria

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const { response } = require("express");
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const db = require("./db/db.json");
+//const db = require("./db/db.json");
 
 //start port at 3001 or variable location
 const PORT = process.env.PORT || 3001;
@@ -17,16 +17,30 @@ app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
+function getDbJson() {
+  return new Promise((resolve, reject) => {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        const dataBase = JSON.parse(data);
+        resolve(dataBase);
+      }
+    });
+  });
+}
+
 //gets notes from database
 app.get("/api/notes", (req, res) => {
-  res.json(db);
+  getDbJson().then((db) => res.json(db));
+  /* console.log("server is responding", JSON.stringify(db)) */
 });
 
 //adds new notes to database.
 app.post("/api/notes", (req, res) => {
   res.json(`${req.method} request recieved`);
 
-  console.info("recieved " + JSON.stringify(req.body));
+  console.info("recieved post req", JSON.stringify(req.body));
 
   const readAppend = (content, file) => {
     fs.readFile(file, "utf8", (err, data) => {
@@ -35,7 +49,9 @@ app.post("/api/notes", (req, res) => {
       } else {
         //assign random id to object
         content.id = Math.floor(Math.random() * 1000000000000 + 1);
+        console.log("data is ", data);
         let parsedData = JSON.parse(data);
+        console.log("parsed data is ", typeof parsedData, parsedData);
         parsedData.push(content);
 
         writeToFile(file, parsedData);
@@ -58,15 +74,17 @@ app.delete("/api/notes/:id", (req, res) => {
   res.status(200).send("recieved Delete request");
   const toDelete = req.params.id;
   //loops through object array to match ID with object and removes object with splice.
-  for (let index = 0; index < db.length; index++) {
-    const element = db[index];
-    if (element.id == toDelete) {
-      db.splice(index, 1);
+  getDbJson().then((db) => {
+    for (let index = 0; index < db.length; index++) {
+      const element = db[index];
+      if (element.id == toDelete) {
+        db.splice(index, 1);
+      }
     }
-  }
-  fs.writeFile("./db/db.json", JSON.stringify(db, null, 4), (err) =>
-    err ? console.error(err) : console.info(`\nData written to `)
-  );
+    fs.writeFile("./db/db.json", JSON.stringify(db, null, 4), (err) =>
+      err ? console.error(err) : console.info(`Data written`)
+    );
+  });
 });
 
 app.get("*", (req, res) => res.sendFile(path.join(__dirname, "/index.html")));
